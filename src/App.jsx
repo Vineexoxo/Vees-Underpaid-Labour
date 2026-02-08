f877a2f0bc11a23298be47ba8af64bd2d0bc58ac
Vineexoxo <vineetpriyedarshi@gmail.com>
2026-02-08 21:21:25 +0530

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Book from './components/Book';
 import FeedbackPage from './pages/FeedbackPage';
-import { pagesData } from './data/pagesData';
+import { pagesData } from './data/pagesData.js';
 
 function App() {
   return (
